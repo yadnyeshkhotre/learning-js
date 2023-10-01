466b0f01d416eb8c12c9a4fa242f3d17d90f95b2
yadnyeshkhotre <96002336+yadnyeshkhotre@users.noreply.github.com>
2023-10-01 18:15:24 +0000

    conversion operation perform

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
-// let score=undefined
+let score=undefined
 
-// let valueNumber=score
+let valueNumber=score
 
-// console.log(typeof valueNumber);
+console.log(typeof valueNumber);
 
-// console.log(valueNumber);
+console.log(valueNumber);
 
 /*
 
@@ -31,4 +31,46 @@ true=>1;  false=>0
 // let string="iAmAString"
 // let stringNumber=String(string)
 // console.log(typeof(stringNumber))
+
+// ********************************Operations*************************************
+
+// let value=3
+// let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+
+// let str1="hello"
+// let str2='yadnyesh'
+
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log(1+"2"+1)
+// console.log(+true)
+// console.log(+"")
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.table([num1,num2,num3])
+
+// let gameCounter=100
+// gameCounter++//101
+// ++gameCounter//101
+// console.log(gameCounter)
+
+//prefix and postfix
+// let x = 3;
+// const y = x++;
+// console.log(`x=${x}, y=${y}`);
+
+// let a = 3;
+// const b = ++a;
+// console.log(`a=${a}, b=${b}`);
 
