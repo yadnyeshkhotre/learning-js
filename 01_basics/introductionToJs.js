be4494f61552c5384c9b02a4cd431ea37c6d0e8e
yadnyeshkhotre <smilingyadnyesh001@gmail.com>
2023-10-04 00:54:46 +0530

    doing the course again

---
+++ 01_basics/introductionToJs.js
@@ -0,0 +1,4 @@
+//nodejs and denojs were both created by Ryan Dahl. He initially developed nodejs and lateer went on to create denojs. nodejs is a popular js runtime that allows developers to run server-side javascript, while <denojs is a secure runtime >for js and typescript
+
+
+//creating a new github directory and opening the project online on github codespace: create new repo-open codespace-vscode-view-command palette-container-1st opt-nodejs-showall-nodejs & js-18-OK.
