9e96a3f255a98b4b01f92225769113a37de05fc5
yadnyeshkhotre <smilingyadnyesh001@gmail.com>
2023-10-17 17:38:02 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -4,10 +4,9 @@ based on kis tarah se data ko memory me rakha jata hain aur access kiya jata hai
 2.non-primitive
 */
 
-//Primitive: String, Number, Boolean, Null, Undefined, BigInt, Symbol
+//Primitive (copy) : String, Number, Boolean, Null, Undefined, BigInt, Symbol
 
-//Non-Primitive (reference)
-//Array, Objects, Function
+//Non-Primitive (reference) : Array, Objects, Functions
 
 /*
 If in any langauge we dont have to specifically mention the datatype of the variable, that language is know as dynamic which means we can change the value during runtime.
@@ -18,13 +17,15 @@ JavaScript is a dynamically typed language
 //symbols
 // const id=Symbol('124')
 // const anotherId=Symbol('124')
-
 // console.log(id===anotherId)
 
-// //BitInt
-// const bigNumber=234374757483447448378373838n
-// console.log(typeof bigNumber)
 
+// //BitInt
+const bigNumber=234374757483447448378373838n
+console.log(typeof bigNumber)
+
+
+// non-primitive
 // //array
 // let arr=["mango","banana","tomato"]
 // console.log(typeof arr); // Outputs: 'object'
@@ -32,12 +33,11 @@ JavaScript is a dynamically typed language
 
 
 // //object
-// let myObj={
-//     name:"yadnyesh",
-//     age:22,
-//     city:"Banglore"
-// }
-
+let myObj={
+    name:"yadnyesh",
+    age:22,
+    city:"Banglore"
+}
 // console.log(typeof myObj)
 
 // //function
