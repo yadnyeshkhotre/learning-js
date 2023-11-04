ef0cbfa4e702cbb16ef8a726fbf4650c0a560c62
Yadnyesh Khotre <96002336+yadnyeshkhotre@users.noreply.github.com>
2023-11-04 17:11:49 +0000

    arrowfun

--- 02_basics/07_arrowFunction.js
+++ 02_basics/07_arrowFunction.js
@@ -1,67 +1,72 @@
-let family={
-    father:"Pappa",
-    familyHead:"mother",
-    totalEarnings:30000,
-    earnings:function(){
-        console.log(`${this.totalEarnings}, is the total earning`)
-        console.log(this)
-    },
-}
-
-// console.log(family)
-// family.earnings()
-
-// console.log(this)
-
-
-//this in function
-function coffee(){
-    let randomVar=55
-    console.log(this.randomVar)
-    console.log(this)
-}
-coffee()
-
-//arrow function
-const milk =()=>{
-    let randomVar=55
-    console.log(this.randomVar)
-    console.log(this)
-}
-milk()
-
-//In both the cases may it be arrow fun or normal fun we cannot use this keyword to access the values, in normal fun with the help of this we can see lot many methods of it
-
-
-// const multiply=(a,b)=>{
-//     return a*b
+// let family={
+//     father:"Pappa",
+//     familyHead:"mother",
+//     totalEarnings:30000,
+//     earnings:function(){
+//         console.log(`${this.totalEarnings}, is the total earning`)
+//         console.log(this)
+//     },
 // }
+
+// // console.log(family)
+// // family.earnings()
+
+// // console.log(this)
+
+
+// //this in function
+// function coffee(){
+//     let randomVar=55
+//     console.log(this.randomVar)
+//     console.log(this)
+// }
+// coffee()
+
+// //arrow function
+// const milk =()=>{
+//     let randomVar=55
+//     console.log(this.randomVar)
+//     console.log(this)
+// }
+// milk()
+
+// //In both the cases may it be arrow fun or normal fun we cannot use this keyword to access the values, in normal fun with the help of this we can see lot many methods of it
+
+
+// // const multiply=(a,b)=>{
+// //     return a*b
+// // }
+// // console.log(multiply(11,11))
+
+
+// const multiply=(a,b)=> ( a*b)
 // console.log(multiply(11,11))
 
+// //how to return object with arrow function
+// const obj={
+//     name:"yadnyesh",
+//      rollNo:36
+// }
 
-const multiply=(a,b)=> ( a*b)
-console.log(multiply(11,11))
+// let fun=()=>obj.name
+// console.log(fun())
 
-//how to return object with arrow function
-const obj={
-    name:"yadnyesh",
-     rollNo:36
-}
+// const fun2=()=>({
+//     myName:"yadnyesh",
+//     mummyName:"jayshri"
+// })
 
-let fun=()=>obj.name
-console.log(fun())
-
-const fun2=()=>({
-    myName:"yadnyesh",
-    mummyName:"jayshri"
-})
-
-console.log(fun2())
+// console.log(fun2())
 
 
 
 
 //IIFE ...global scope ke pollution se problem ho rahi thi issiliye humne IIFE use kiya (Immediately Invoked Function Expressions)
-(function chai(){
-    console.log("connected");
-})()
+(function addTwoNum(a,b){
+    console.log(`the addition of ${a} and ${b} is ${a+b} `);
+})(5,5);
+
+
+((name)=>{
+    console.log(`go to sleep ${name}`)
+})("yadnyesh")
