221cb4bd7c2255b1e8735ec62b1549aec9246f66
Yadnyesh Khotre <96002336+yadnyeshkhotre@users.noreply.github.com>
2023-11-04 10:06:00 +0000

    scope

---
+++ 02_basics/06_scopes.js
@@ -0,0 +1,19 @@
+// let a=300
+if (true){
+    let a=10
+    const b=20
+    var c=30
+}
+
+// console.log(a);error
+// console.log(b);error
+console.log(c);
+
+
+
+var d=30
+console.log(d)
+if(true){
+    console.log("inside",d)
+}
+console.log("outside",d) //global values also get updated.
